a774ed0531825c7cf49117159b85dd444529e705
OdedHol <128286683+OdedHol@users.noreply.github.com>
2023-06-28 15:42:15 +0300

    Update Under_water_sensors.ino

--- Codes/Under_water_sensors.ino
+++ Codes/Under_water_sensors.ino
@@ -1,173 +1,105 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+#include <Adafruit_MLX90614.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
 #include "ThingSpeak.h"
 
-// WiFi
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+WiFiClient client;
+
+// Set the ThingSpeak channel and API key information
+unsigned long myChannelNumber = 2184926;
+const char* myWriteAPIKey = "JDZ7WLK4UNQE5CEW";
+
+// Set the WiFi network credentials
 const char* ssid = "TP-Link_905D";
 const char* password = "33072036";
 
-// MQTT
-const char* mqttServer = "broker.hivemq.com";
-int mqttPort = 1883;
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-
-// ThingSpeak
-unsigned long myChannelNumber = 2184926;
-const char* myWriteAPIKey = "JDZ7WLK4UNQE5CEW";
+// Set the ThingSpeak server address
 const char* server = "api.thingspeak.com";
-int wait_between_uploads = 10000;
-WiFiClient client;
 
-// PH
-const int pHPin = A2;
-const int pH_V = D13;
-float ph;
-float Value = 0;
-
-// Temperature
-const int SENSOR_PIN = A4;
-const int SENSOR_V = D12;
-float tempCelsius;
-float tempFahrenheit;
-
-// EC
-int TDS_PIN = A1;
-int TDS_V = D11;
-int val;
-float voltage;
-int EC = 0;
-
-OneWire oneWire(SENSOR_PIN);
-DallasTemperature tempSensor(&oneWire);
-
-long previous_time = 0;
-String msgString;
-
-void setupMQTT() {
-  mqttClient.setServer(mqttServer, mqttPort);
-}
-
-void reconnect() {
-  Serial.println("Connecting to MQTT Broker...");
-  while (!mqttClient.connected()) {
-    Serial.println("Reconnecting to MQTT Broker..");
-    String clientId = "ESP32Client-";
-    clientId += String(random(0xffff), HEX);
-
-    if (mqttClient.connect(clientId.c_str())) {
-      Serial.println("Connected.");
-    }
-  }
-}
+// Set the time to wait between uploading data to ThingSpeak (in milliseconds)
+int wait_between_uploads = 10000; // 10 seconds
 
 void setup() {
-  // WiFi
+  Serial.begin(115200);
+
+  if (!sht31.begin(0x44)) {
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  mlx.begin();
+
+  // Disconnect any previous WiFi connection
+  WiFi.disconnect();
+  delay(10);
+
+  // Connect to the WiFi network
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("Connected to Wi-Fi");
+  Serial.print("ESP32 connected to WiFi: ");
+  Serial.println(ssid);
+  Serial.println();
 
-  // ThingSpeak
+  // Initialize the ThingSpeak library with the WiFi client
   ThingSpeak.begin(client);
-
-  // MQTT
-  setupMQTT();
-
-  // PH
-  Serial.begin(115200);
-  pinMode(pH_V, OUTPUT);
-  digitalWrite(pH_V, LOW);
-  delay(1000);
-
-  // Temperature
-  tempSensor.begin();
-  pinMode(SENSOR_V, OUTPUT);
-  digitalWrite(SENSOR_V, LOW);
-
-  // EC
-  pinMode(TDS_V, OUTPUT);
-  digitalWrite(TDS_V, LOW);
 }
 
 void loop() {
-  if (!mqttClient.connected())
-    reconnect();
-  mqttClient.loop();
-  long now = millis();
+  // Read SHT31 sensor values
+  float shtTemperature = sht31.readTemperature();
+  float shtHumidity = sht31.readHumidity ();
+  float mlxambient = mlx.readAmbientTempC();
+  float mlxobject = mlx.readObjectTempC();
 
-  if (now - previous_time > 1000) {
-    previous_time = now;
-
-    // PH
-    digitalWrite(pH_V, HIGH);
-    delay(1000);
-    Value = analogRead(pHPin);
-    Serial.print(Value);
-    Serial.print(" | ");
-    voltage = Value * (3.3 / 4095.0);
-    ph = (3.3 * voltage);
-    Serial.println(ph);
-    digitalWrite(pH_V, LOW);
-
-    // Temperature
-    digitalWrite(SENSOR_V, HIGH);
-    delay(1000);
-    tempSensor.requestTemperatures();
-    tempCelsius = tempSensor.getTempCByIndex(0);
-    tempFahrenheit = tempCelsius * 9 / 5 + 32;
-    Serial.print("Temperature: ");
-    Serial.print(tempCelsius);
-    Serial.print("°C");
-    Serial.print("  ~  ");
-    Serial.print(tempFahrenheit);
-    Serial.println("°F");
-    digitalWrite(pH_V, LOW);
-
-    // EC
-    digitalWrite(TDS_V, HIGH);
-    delay(1000);
-    val = analogRead(TDS_PIN);
-    voltage = (3.3 / 4095) * val;
-    EC = voltage * 1000;
-    Serial.print("EC: ");
-    Serial.print(EC);
-    digitalWrite(TDS_V, LOW);
-    Serial.print("DONE");
-
-
-    // ThingSpeak
-    ThingSpeak.setField(6, ph);
-    ThingSpeak.setField(7, tempCelsius);
-    ThingSpeak.setField(8, EC);
-    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-    Serial.println("Uploaded to ThingSpeak server.");
-    client.stop();
-
-    // MQTT
-    char tempString[8];
-    dtostrf(tempCelsius, 1, 2, tempString);
-     if (ph<5.5) {
-    mqttClient.publish("agrotech/2023/relay/1", "1");
-     }
-      if (ph>5.5) {
-    mqttClient.publish("agrotech/2023/relay/1", "0");
-     }
-     if (EC<500) {
-    mqttClient.publish("agrotech/2023/relay/2", "1");
+  if (!isnan(shtTemperature)) {
+    Serial.print("SHT Temp *C = ");
+    Serial.println(shtTemperature);
+  } else {
+    Serial.println("Failed to read SHT temperature");
   }
-    if (EC>500) {
-    mqttClient.publish("agrotech/2023/relay/2", "0");
-  }
-  if (Serial.print("DONE")) {
-    mqttClient.publish("agrotech/2023/relay/3", "1");
-  }
-      delay(300000);
 
-}
+  if (!isnan(shtHumidity)) {
+    Serial.print("SHT Hum. % = ");
+    Serial.println(shtHumidity);
+  } else {
+    Serial.println("Failed to read SHT humidity");
+  }
+
+  // Read MLX90614 sensor values
+  Serial.print("Ambient = ");
+  Serial.print(mlx.readAmbientTempC());
+  Serial.print("*C\tObject = ");
+  Serial.print(mlx.readObjectTempC());
+  Serial.println("*C");
+
+  // Set the values to be sent to ThingSpeak
+  ThingSpeak.setField(1, shtTemperature);
+  ThingSpeak.setField(2, shtHumidity);
+  ThingSpeak.setField(3, mlxambient);
+  ThingSpeak.setField(4, mlxobject);
+
+  // Send the data to ThingSpeak
+  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+  // Print a message to the serial monitor indicating that the data has been uploaded
+  Serial.println("Uploaded to ThingSpeak server.");
+
+  // Disconnect the WiFi client
+  client.stop();
+
+  // Wait for the specified amount of time before uploading the next set of data
+  Serial.println("Waiting to upload next reading...");
+  Serial.println();
+
+  delay(wait_between_uploads);
 }
